6c7b10c9f2853d4e7339ed9960a6a7f22e430a5d
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-05-18 08:36:42 -0400

    remove Pods, edit layoutSubviews and init coder

--- GaugeKit/Gauge.swift
+++ GaugeKit/Gauge.swift
@@ -241,7 +241,7 @@ public class Gauge: UIView {
     }
 
     required public init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        super.init(coder: aDecoder)!
         updateLayerProperties()
     }
 
@@ -277,7 +277,7 @@ public class Gauge: UIView {
 
     public override func layoutSubviews() {
         resetLayers()
-        gaugeLayer = getGauge(rotateAngle: rotate / 10 * M_PI)
+        gaugeLayer = getGauge(rotate / 10 * M_PI)
         layer.addSublayer(gaugeLayer)
         updateLayerProperties()
     }
