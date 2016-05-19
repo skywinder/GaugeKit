a8519d433756e17bc274647ee5b2b475f098be4e
Sinan Ulkuatam <sulkuatam@gmail.com>
2016-05-19 08:34:22 -0400

    remove podfile, add as embedded framework

---
+++ Example/Example/GaugeKit.framework/Headers/GaugeKit.h
@@ -0,0 +1,19 @@
+//
+//  GaugeKit.h
+//  GaugeKit
+//
+//  Created by Petr Korolev on 17/06/15.
+//  Copyright (c) 2015 Petr Korolev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GaugeKit.
+FOUNDATION_EXPORT double GaugeKitVersionNumber;
+
+//! Project version string for GaugeKit.
+FOUNDATION_EXPORT const unsigned char GaugeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GaugeKit/PublicHeader.h>
+
+
